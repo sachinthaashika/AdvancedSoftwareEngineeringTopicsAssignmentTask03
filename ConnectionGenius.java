f43d1925c4707effd8b9a047d875af69861452fc
sachinthaashika <86411401+sachinthaashika@users.noreply.github.com>
2022-06-16 01:46:57 +0530

    Designed Strategy Design Pattern

---
+++ ConnectionGenius.java
@@ -0,0 +1,36 @@
+package com.game.abominodo4.service;
+/*
+ * @author Sachintha Ashika Bandara Yapa
+ */
+import java.net.InetAddress;
+
+
+public class ConnectionGenius {
+
+  InetAddress ipa;
+  
+  public ConnectionGenius(InetAddress ipa) {
+    this.ipa = ipa;
+  }
+
+  public void fireUpGame() {
+    downloadWebVersion();
+    connectToWebService();
+    awayWeGo();
+  }
+  
+  public void downloadWebVersion(){
+    System.out.println("Getting specialised web version..");
+    System.out.println("Wait a couple of moments");  
+  }
+  
+  public void connectToWebService() {
+    System.out.println("Connecting");
+  }
+  
+  public void awayWeGo(){
+    System.out.println("Ready to play");
+  }
+
+}
+    
---
+++ IOSpecialist.java
@@ -0,0 +1,14 @@
+package com.game.abominodo4.service;
+/*
+ * @author Sachintha Ashika Bandara Yapa
+ */
+import com.game.abominodo4.view.IOLibrary;
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
+   
